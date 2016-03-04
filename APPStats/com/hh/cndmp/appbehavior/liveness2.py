f3d56f61942aacbf312e0031fc9a712103780f17
黄绿君 <hlj@haohandata.com.cn>
2016-03-04 19:24:06 +0800

    add

--- APPStats/com/hh/cndmp/appbehavior/liveness2.py
+++ APPStats/com/hh/cndmp/appbehavior/liveness2.py
@@ -156,6 +156,12 @@ result_24 = '|'.join(['红孩子母婴', date, str(pv_24), str(uv_24)])
 result_25 = '|'.join(['母婴之家', date, str(pv_25), str(uv_25)])
 result_26 = '|'.join(['母爱街', date, str(pv_26), str(uv_26)])
 
+out = '\n'.join([result_1, result_2, result_3, result_4, result_5, result_6, result_7, result_8,
+                 result_9, result_10, result_11, result_12, result_15, result_16, result_17, result_18,
+                 result_19, result_20, result_21, result_22, result_23, result_24, result_25, result_26])
+with open('F:\cndmp\APPStats\com\hh\cndmp\\appbehavior\\appstats_20150812.txt', 'a') as output_file:
+    output_file.write(out)
+
 print(result_1)
 print(result_2)
 print(result_3)
